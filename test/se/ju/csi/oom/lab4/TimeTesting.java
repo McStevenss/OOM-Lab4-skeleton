fe99b885e184028642f278e7631a317409be95fc
Måns Svensson <mans.cool@hotmail.com>
2018-10-10 08:40:04 +0200

    added test file

---
+++ test/se/ju/csi/oom/lab4/TimeTesting.java
@@ -0,0 +1,19 @@
+package se.ju.csi.oom.lab4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TimeTesting {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
